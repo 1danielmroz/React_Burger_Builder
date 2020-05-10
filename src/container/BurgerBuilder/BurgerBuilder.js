b9279cdb590cde19e13a2816b7f582bf06cabcb1
1danielmroz <danielmroz@protonmail.com>
2020-05-10 11:07:02 +0200

    Upload project

---
+++ src/container/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+import Aux from "../../hoc/Aux";
+import Burger from "../../components/Burger/Burger"
+import BuildControls from "../../components/Burger/BuildControls/BuildControls";
+
+const INGREDIENT_PRICES={
+    salad:0.5,
+    chesee:0.4,
+}
+
+class BurgerBuilder extends Component {
+     
+    state ={
+        ingredients:{
+            salad:0,
+            bacon:0,
+            cheese:0,
+            meat:0
+        },
+        totalPrice:4
+    }
+
+    addIngredientHandler = (type) =>{
+        const oldCount = this.state.ingredients[type];
+        const updatedCounted = oldCount+1;
+        const updatedIngredients ={
+            ...this.state.ingredients
+        };
+        updatedIngredients[type] =updatedCounted;
+
+    }
+
+    render() {
+        return (
+            <Aux>
+                <Burger ingredients={this.state.ingredients}/>
+                <BuildControls />
+            </Aux>
+        );
+    }
+}
+
+
+export default BurgerBuilder;
